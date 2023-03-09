a3ff25f23c5cfa2ef17a245f7ba45c51b6363594
seungmin <fsjki0328@naver.com>
2023-03-09 18:07:50 +0900

    first update

---
+++ docker_install.sh
@@ -0,0 +1,20 @@
+# docker_install.sh
+#!/bin/bash
+
+# basic update
+sudo apt-get update -y && sudo apt upgrade -y
+
+
+# install docker
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+
+sudo apt update -y
+
+apt-cache policy docker-ce
+
+sudo apt install -y docker-ce
+
+sudo usermod -aG docker $USER
