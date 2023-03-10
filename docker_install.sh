3da987e8384000a7b5ccf8e3e49e075e9d742ebc
Luji <fsjki0328@naver.com>
2023-03-10 17:46:39 +0900

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -18,3 +18,13 @@ apt-cache policy docker-ce
 sudo apt install -y docker-ce
 
 sudo usermod -aG docker $USER
+
+
+
+# install docker-compose
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
